6d21bb015187767b3c49ae99e74942d7997b1a0d
schadof <lars.ragutt@gmx.ch>
2017-06-16 16:19:00 +0200

    Classes as per UML

--- src/PresentationAdministration.java
+++ src/PresentationAdministration.java
@@ -1,2 +1,22 @@
+import java.text.SimpleDateFormat;
+
 public class PresentationAdministration {
+
+    public PresentationAdministration(){
+
+    }
+
+    public void createPresentation(Movie movie, CinemaRoom cinemaRoom, SimpleDateFormat simpleDateFormat){
+
+    }
+    public Presentation getPresentation(String string, SimpleDateFormat simpleDateFormat, CinemaRoom cinemaRoom){
+
+    }
+    public void editPresentation(String string, SimpleDateFormat simpleDateFormat, CinemaRoom cinemaRoom){
+
+    }
+    public void removePresentation(String string, SimpleDateFormat simpleDateFormat, CinemaRoom cinemaRoom){
+
+    }
+
 }
