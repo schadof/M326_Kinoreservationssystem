0f8f77c6027e5248724b421de3a637352175a04c
Globidolf <silvan.pfister@hotmail.com>
2017-06-20 14:34:29 +0200

    Added functionality to the Client class
    Finally managed to get GIT to work with intellij

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,25 @@
 public class Client {
-    public void Client(){
-
+    private String fullName;
+    private  String phoneNumber;
+    private String eMail;
+    private String address;
+    public void Client(String Name, String Number, String Email, String Address){
+        fullName = Name;
+        phoneNumber = Number;
+        eMail = Email;
+        address = Address;
     }
 
     public String getName(){
-
+        return fullName;
     }
     public String getPhone(){
-
+        return phoneNumber;
     }
     public String getMail(){
-
+        return eMail;
     }
     public String getAddress(){
-
+        return address;
     }
 }
