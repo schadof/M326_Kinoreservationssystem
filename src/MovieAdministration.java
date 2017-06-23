747f8c6c0f27a610130730cfe802caba5a79e7e6
Globidolf <silvan.pfister@hotmail.com>
2017-06-23 14:08:52 +0200

    Pre gitignore fix

--- src/MovieAdministration.java
+++ src/MovieAdministration.java
@@ -5,4 +5,9 @@ public class MovieAdministration {
     public MovieAdministration(){
         movies = new Movie[0];
     }
+
+    public Movie[] getAllMovies(){ return movies; }
+
+    public void addMovie(Movie movie){movies = Utility.Array_Add(movies, movie);}
+
 }
