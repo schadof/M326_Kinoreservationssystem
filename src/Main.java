8b77048e444cf28ef3f6d06a37b5af4b4ff061a6
schadof <lars.ragutt@gmx.ch>
2017-06-27 12:34:18 +0200

    Tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.time.Duration;
-
 import static java.time.temporal.ChronoUnit.HOURS;
 
 public class Main {
@@ -7,6 +6,10 @@ public class Main {
         CinemaManagement cinemaManagement = new CinemaManagement();
         Movie movie = new Movie("Test", Duration.of(2, HOURS),"This is a test");
         cinemaManagement.getMovieAdmin().addMovie(movie);
-        System.out.println(cinemaManagement.getMovieAdmin().getAllMovies());
+        ClientAdministration clientAdministration = new ClientAdministration();
+        Client client = new Client("Lars","1234567","Test@test.ch","Zuhause");
+        clientAdministration.addClient(client);
+        System.out.println(cinemaManagement.getMovieAdmin().getAllMovies().get(0).getTitle());
+        System.out.println(clientAdministration.getClientByName("Lars").getAddress());
     }
 }
